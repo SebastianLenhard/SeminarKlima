f08bc1cfe864e12d4e7874f1c12705018431c7c3
DavyJonesCH <jonas.gruendler@gmail.com>
2018-06-06 10:27:19 +0200

    first text, more code

--- skript/thermohalin/Code/thc_groupim_3b1f.m
+++ skript/thermohalin/Code/thc_groupim_3b1f.m
@@ -3,7 +3,7 @@
 
 %% Startvariablen, Timebase, Umgebungskonstanten
 
-y0 = [5; 5; 5; 0.032; 0.035; 0.042;];
+y0 = [5; 5; 5; 0.032; 0.032; 0.042;];
 tspan = [0 90000];
 val = [0; 0; 0; 0.03; 0.03; 0.03];
 
@@ -16,10 +16,10 @@ b = 8*10^(-4);
 results = zeros(4,1);
 counter = 0;
 
-iinc = (20)/5;
-jinc = (20)/5;
-kinc = (0.05-0.03)/5;
-linc = (0.05-0.03)/5;
+iinc = (20)/10;
+jinc = (20)/10;
+kinc = (0.05-0.03)/10;
+linc = (0.05-0.03)/10;
 
 
 for i = 0:iinc:25
@@ -27,9 +27,9 @@ for i = 0:iinc:25
         for k = 0.03:kinc:0.05 
             for l = 0.03:linc:0.05
                 [t,y] = ode45(@(t,y) odefun_3Box_1Flux(t,y,val), tspan, y0);
-                q = k*(a*(y(:,2)-y(:,1))-b*(y(:,4)-y(:,3)));
+                q = k*(a*(y(:,2)-y(:,1))-b*(y(:,5)-y(:,4)));
                 if(q(end)<0)
-                    results = [results [i;j;k;l]]
+                    results = [results [i;j;k;l]];
                 end
                 counter = counter + 1;
                 val(6) = val(6) + linc;               
@@ -44,3 +44,5 @@ for i = 0:iinc:25
     val(3) = 5;
 end
 val(1) = 5;
+
+save('groupsim_ws')
